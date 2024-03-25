b2f0f9e76bcc9adfa003d411a8f97be805395ab5
Brady Holt <brady.holt@gmail.com>
2024-03-25 12:28:07 -0500

    Make change for interop with CommonJS better

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,10 +2,9 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts"],
-  format: ["esm", "cjs", "iife"],
-  legacyOutput: true,
+  format: ["esm", "cjs", "iife"],  
   splitting: false,
   sourcemap: true,
   clean: true,
-  dts: true,
+  dts: true 
 });
